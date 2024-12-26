94c7cbbbb4d4a4f815820b2a7d3dbc364722f64c
pokeum <pokeum@ab180.co>
2024-12-26 20:18:09 +0900

    add. iOS plist updater plugin

---
+++ Line_SDK_Unreal/Plugins/IosPListUpdater/Source/IosPListUpdater/IosPListUpdater.cpp
@@ -0,0 +1,15 @@
+#include "IosPListUpdater.h"
+
+#define LOCTEXT_NAMESPACE "FIosPListUpdaterModule"
+
+void FIosPListUpdaterModule::StartupModule()
+{
+}
+
+void FIosPListUpdaterModule::ShutdownModule()
+{
+}
+
+#undef LOCTEXT_NAMESPACE
+
+IMPLEMENT_MODULE(FIosPListUpdaterModule, IosPListUpdater)
